a917a6651dac75fad682be2f39cc39cf46ee0ff6
priyankastha38 <49015933+priyankastha38@users.noreply.github.com>
2021-03-30 12:38:24 +0545

    changes in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ void main(){
      MaterialApp(
        home: Scaffold(
          appBar: AppBar(
-           title: Text('app1'),
+           title: Text('DEMO APP'),
          ),
        ),
      )
